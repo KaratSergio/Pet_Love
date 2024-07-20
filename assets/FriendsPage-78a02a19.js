2a80415dd2dccb8f9b7817d3f3ebcad91931eb54
KaratSergio <137483700+KaratSergio@users.noreply.github.com>
2024-07-20 11:58:33 +0000

    Deploying to gh-pages from @ KaratSergio/Pet_Love@103a2b4049ebaec33374cca0567d282653b36241 🚀

---
+++ assets/FriendsPage-78a02a19.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-1cef665c.js";const t=()=>s.jsx("section",{className:"m-auto max-w-desktop p-8",children:s.jsx("h2",{className:"text-[54px]",children:"Our friends"})});export{t as default};
--- assets/NewsPage-526cae67.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-fb9d391c.js";const t=()=>s.jsx("section",{className:"m-auto max-w-desktop p-8",children:s.jsx("h2",{className:"text-[54px]",children:"News"})});export{t as default};

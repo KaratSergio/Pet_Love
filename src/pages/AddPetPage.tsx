9c724d23d442d19b7b951c9869535375d0fa3eb9
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-07-27 11:29:46 +0300

    add PetImages

--- src/pages/AddPetPage.tsx
+++ src/pages/AddPetPage.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import PetBlock from '@components/Custom/PetBlock';
+import addPetImages from '@src/assets/img/addPetPage/addPetImages';
 
 const AddPetPage: React.FC = () => {
   return (
-    <section>
-      <p>Add Pet</p>
+    <section className="mx-auto bg-orange-50 p-8">
+      <div className="flex gap-8">
+        <PetBlock
+          images={addPetImages}
+          imageWidth="500px"
+          imageHeight="630px"
+          className="w-full max-w-[592px] h-[654px] rounded-60"
+        />
+      </div>
     </section>
   );
 };

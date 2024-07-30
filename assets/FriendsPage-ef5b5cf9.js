cb626268b4d88658e93cb44cdfce600c6839a51d
KaratSergio <137483700+KaratSergio@users.noreply.github.com>
2024-07-30 11:41:25 +0000

    Deploying to gh-pages from @ KaratSergio/Pet_Love@84178f3cf09af6c941c70a6bf8ab939bf072ad72 🚀

--- assets/FriendsPage-ef5b5cf9.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-c6290245.js";import{T as e}from"./Title-61926f61.js";const r=()=>s.jsx("section",{className:"m-auto max-w-desktop p-8 bg-orange-50",children:s.jsx(e,{})});export{r as default};
